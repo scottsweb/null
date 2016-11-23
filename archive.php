b1445e45d033c105d5b96c89c86b333f87a320a0
Scott Evans <git@scott.ee>
2016-11-23 11:46:11 +0000

    Use the_archive_title and description tags in archive.php

--- archive.php
+++ archive.php
@@ -6,23 +6,10 @@
 
 		<?php tha_content_top(); ?>
 
-		<?php /** if this is a category archive **/ if (is_category()) { ?>
-		<h2 class="page-title"><?php _e('Blog:', 'null'); ?> <?php echo single_cat_title(); ?></h2>
-		<?php /** if this is a tag archive **/ } else if (is_tag()) { ?>
-		<h2 class="page-title"><?php _e('Tag Archive for', 'null'); ?> <span>&ldquo;<?php single_tag_title() ?>&rdquo;</span></h2>
-		<?php /** if this is a daily archive **/ } else if (is_day()) { ?>
-		<h2 class="page-title"><?php the_time('F jS, Y'); ?> <?php _e('Archive', 'null'); ?></h2>
-		<?php /** if this is a monthly archive **/ } else if (is_month()) { ?>
-		<h2 class="page-title"><?php the_time('F, Y'); ?> <?php _e('Archive', 'null'); ?></h2>
-		<?php /** if this is a yearly archive **/ } else if (is_year()) { ?>
-		<h2 class="page-title"><?php the_time('Y'); ?> <?php _e('Archive', 'null'); ?></h2>
-		<?php /** if this is a search **/ } elseif (is_search()) { ?>
-		<h2 class="page-title"><?php _e('Search Results', 'null'); ?></h2>
-		<?php /** if this is an author archive **/ } else if (is_author()) { ?>
-		<h2 class="page-title"><?php _e('Author Archive', 'null'); ?></h2>
-		<?php /** if this is a paged archive **/ } else if (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-		<h2 class="page-title"><?php _e('Blog Archive', 'null'); ?></h2>
-		<?php } ?>
+		<?php
+			the_archive_title( '<h2 class="page-title">', '</h2>' );
+			the_archive_description( '<section class="archive-meta">', '</section>' );
+		?>
 
 		<?php get_template_part('loop', 'archive'); ?>
 
